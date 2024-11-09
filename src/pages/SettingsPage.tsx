5492a1daa4623efedfd6d3d5cc0e10dfa2b49d89
John <johnwis@udel.edu>
2024-11-08 23:24:39 -0500

    adding pages

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,11 @@
+// src/pages/SettingsPage.tsx
+import React from 'react';
+
+const SettingsPage: React.FC = () => (
+  <div>
+    <h2>Settings</h2>
+    <p>Settings content here...</p>
+  </div>
+);
+
+export default SettingsPage;
